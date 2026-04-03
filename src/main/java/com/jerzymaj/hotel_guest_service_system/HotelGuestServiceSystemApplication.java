283b58fe6006caad737f56d9516cbafb3881bd03
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-04-03 11:25:59 +0200

    feat: initial project setup, created necessary package folders, initial application.yaml setup

---
+++ src/main/java/com/jerzymaj/hotel_guest_service_system/HotelGuestServiceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jerzymaj.hotel_guest_service_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelGuestServiceSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelGuestServiceSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jerzymaj/hotel_guest_service_system/HotelGuestServiceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerzymaj.hotel_guest_service_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelGuestServiceSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
