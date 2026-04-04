9dda9e8df359d4df208d9b500af5427f091ffe81
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-04-04 13:00:11 +0200

    feat: add externalized CORS configuration using CorsConfigurationSource

---
+++ src/main/java/com/jerzymaj/hotel_guest_service_system/configuration/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.jerzymaj.hotel_guest_service_system.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.*;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${app.http.allowed-origins}")
+    private String allowedOrigins;
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(allowedOrigins));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
