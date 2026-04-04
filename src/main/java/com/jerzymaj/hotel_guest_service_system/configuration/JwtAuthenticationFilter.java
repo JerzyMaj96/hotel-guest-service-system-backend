b7115e504f62ab0cf402fe4de43da8041b60f2d2
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-04-04 14:25:22 +0200

    feat: add AppConfig and SecurityConfig, JwtAuthenticationFilter extends OnePerRequestFilter now

--- src/main/java/com/jerzymaj/hotel_guest_service_system/configuration/JwtAuthenticationFilter.java
+++ src/main/java/com/jerzymaj/hotel_guest_service_system/configuration/JwtAuthenticationFilter.java
@@ -13,13 +13,14 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtProvider tokenProvider;
     private final CustomUserDetailsService userDetailsService;
