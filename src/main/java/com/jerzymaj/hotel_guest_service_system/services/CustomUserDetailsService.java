2fdc8b683740809059ac6e64d2c8ad2629ff2018
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-04-04 11:12:00 +0200

    refactor: update UserDetailsService to use email and custom User entity

--- src/main/java/com/jerzymaj/hotel_guest_service_system/services/CustomUserDetailsService.java
+++ src/main/java/com/jerzymaj/hotel_guest_service_system/services/CustomUserDetailsService.java
@@ -1,31 +1,23 @@
 package com.jerzymaj.hotel_guest_service_system.services;
 
-import com.jerzymaj.hotel_guest_service_system.models.User;
 import com.jerzymaj.hotel_guest_service_system.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByName(username)
-                .orElseThrow(() -> new UsernameNotFoundException(username));
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
-        return new org.springframework.security.core.userdetails.User(
-                user.getName(),
-                user.getPassword(),
-                List.of(new SimpleGrantedAuthority("ROLE_USER"))
+        return userRepository.findByName(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email)
         );
     }
 }
