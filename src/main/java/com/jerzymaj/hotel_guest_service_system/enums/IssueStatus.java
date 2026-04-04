1eb47733396d8a5cf4f026f82fe01fb3a15df95c
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-04-04 20:03:11 +0200

    feat: implement IsseStatus and IssueType

---
+++ src/main/java/com/jerzymaj/hotel_guest_service_system/enums/IssueStatus.java
@@ -0,0 +1,7 @@
+package com.jerzymaj.hotel_guest_service_system.enums;
+
+public enum IssueStatus {
+    NEW,
+    OPEN,
+    CLOSED
+}
---
+++ src/main/java/com/jerzymaj/hotel_guest_service_system/enums/IssueType.java
@@ -0,0 +1,6 @@
+package com.jerzymaj.hotel_guest_service_system.enums;
+
+public enum IssueType {
+    TECHNICAL,
+    RECEPTION
+}
