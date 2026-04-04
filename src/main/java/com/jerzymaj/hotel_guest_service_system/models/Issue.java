6749f836a2bd982ec84f7729797a1756189f672f
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-04-04 20:24:50 +0200

    finalize Issue entity with LocalDate/LocalTime and other UI-aligned fields

--- src/main/java/com/jerzymaj/hotel_guest_service_system/models/Issue.java
+++ src/main/java/com/jerzymaj/hotel_guest_service_system/models/Issue.java
@@ -2,15 +2,20 @@ package com.jerzymaj.hotel_guest_service_system.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.jerzymaj.hotel_guest_service_system.enums.IssueStatus;
+import com.jerzymaj.hotel_guest_service_system.enums.IssueType;
+import com.jerzymaj.hotel_guest_service_system.enums.PreferredTimeOption;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Entity
+@Table(name = "issues")
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -22,6 +27,15 @@ public class Issue {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @ToString.Include
     private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private IssueType type;
+
+    @NotBlank
+    @Column(nullable = false)
+    @ToString.Include
+    private String title;
 
     @NotBlank
     @Column(nullable = false, length = 3000)
@@ -36,8 +50,16 @@ public class Issue {
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
+    private PreferredTimeOption preferredTimeOption;
+
+    private LocalDate preferredDate;
+
+    private LocalTime preferredTime;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     @Builder.Default
-    private IssueStatus status = IssueStatus.OPEN;
+    private IssueStatus status = IssueStatus.NEW;
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @CreationTimestamp
