0ccfbe56bd177d03dc3a9a27963d83fb873798d7
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-04-08 21:18:15 +0200

    feat: add findAllIssuesByUserId method in IssueService

--- src/main/java/com/jerzymaj/hotel_guest_service_system/services/IssueService.java
+++ src/main/java/com/jerzymaj/hotel_guest_service_system/services/IssueService.java
@@ -13,6 +13,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class IssueService {
@@ -41,6 +43,10 @@ public class IssueService {
                 .build();
 
         return issueRepository.save(issue);
+    }
+
+    public List<Issue> findAllIssuesByUserId(Long userId) {
+        return issueRepository.findAllByUserIdSortedByDate(userId);
     }
 
     public Long getCurrentUserId() {
