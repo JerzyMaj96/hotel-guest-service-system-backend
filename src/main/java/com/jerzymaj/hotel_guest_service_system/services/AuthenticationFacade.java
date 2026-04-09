976ae59895772d995eef3c945fe63845aa8a873b
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-04-09 12:07:30 +0200

    feat: add new AuthenticationFacade class

---
+++ src/main/java/com/jerzymaj/hotel_guest_service_system/services/AuthenticationFacade.java
@@ -0,0 +1,12 @@
+package com.jerzymaj.hotel_guest_service_system.services;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFacade implements IAuthenticationFacade{
+    @Override
+    public String getAuthenticatedUserEmail() {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
+}
