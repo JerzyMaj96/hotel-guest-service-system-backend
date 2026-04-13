3f4a0bcdde708513e7d67a9818ca8df99fa15f36
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-04-13 20:38:52 +0200

    test: add @ActiveProfile annotation to the main test, it fixes it

--- src/test/java/com/jerzymaj/hotel_guest_service_system/HotelUserServiceSystemApplicationTests.java
+++ src/test/java/com/jerzymaj/hotel_guest_service_system/HotelUserServiceSystemApplicationTests.java
@@ -2,8 +2,10 @@ package com.jerzymaj.hotel_guest_service_system;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HotelUserServiceSystemApplicationTests {
 
     @Test
