7a1dcdf90d471e844192a1d356cd3d90c395c830
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-05-11 17:35:05 +0200

    refactor: handle MessagingException internally

--- src/main/java/com/jerzymaj/hotel_guest_service_system/services/EmailService.java
+++ src/main/java/com/jerzymaj/hotel_guest_service_system/services/EmailService.java
@@ -5,11 +5,13 @@ import com.jerzymaj.hotel_guest_service_system.security.IAuthenticationFacade;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EmailService {
@@ -18,16 +20,20 @@ public class EmailService {
     private final IAuthenticationFacade authenticationFacade;
 
     @Async
-    public void sendNotificationEmail(User user, String title, String description) throws MessagingException {
-        MimeMessage message = javaMailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+    public void sendNotificationEmail(User user, String title, String description) {
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-        helper.setFrom(authenticationFacade.getAuthenticatedUserEmail());
-        helper.setTo("tech@hotel.com");
-        helper.setSubject(title + "-" + user.getFirstName() + " " + user.getLastName());
-        helper.setText(description);
+            helper.setFrom(authenticationFacade.getAuthenticatedUserEmail());
+            helper.setTo("tech@hotel.com");
+            helper.setSubject(title + "-" + user.getFirstName() + " " + user.getLastName());
+            helper.setText(description);
 
-        javaMailSender.send(message);
+            javaMailSender.send(message);
+        } catch (MessagingException ex) {
+            log.error("Failed to send async email notification", ex);
+        }
     }
 
 }
