9093be92d854f9891c0f21944076c17f39942945
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-05-13 07:57:55 +0200

    refactor(email): introduce EmailService interface and move impl to subpackage

---
+++ src/main/java/com/jerzymaj/hotel_guest_service_system/services/impl/EmailServiceImpl.java
@@ -0,0 +1,42 @@
+package com.jerzymaj.hotel_guest_service_system.services.impl;
+
+import com.jerzymaj.hotel_guest_service_system.models.User;
+import com.jerzymaj.hotel_guest_service_system.security.AuthenticationFacade;
+import com.jerzymaj.hotel_guest_service_system.services.EmailService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class EmailServiceImpl implements EmailService {
+
+    private final JavaMailSender javaMailSender;
+    private final AuthenticationFacade authenticationFacade;
+
+
+    @Override
+    @Async
+    public void sendNotificationEmail(User user, String title, String description) {
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setFrom(authenticationFacade.getAuthenticatedUserEmail());
+            helper.setTo("tech@hotel.com");
+            helper.setSubject(title + "-" + user.getFirstName() + " " + user.getLastName());
+            helper.setText(description);
+
+            javaMailSender.send(message);
+        } catch (MessagingException ex) {
+            log.error("Failed to send async email notification", ex);
+        }
+    }
+
+}
--- src/test/java/com/jerzymaj/hotel_guest_service_system/unit_tests/IssueServiceTest.java
+++ src/test/java/com/jerzymaj/hotel_guest_service_system/unit_tests/IssueServiceTest.java
@@ -18,9 +18,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
