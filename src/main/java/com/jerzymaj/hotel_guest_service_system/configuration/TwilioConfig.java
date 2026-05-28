029e201af26d3954f0b84efa70d142bceff0de08
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-05-28 14:39:09 +0200

    feat: add twilio confguration class

---
+++ src/main/java/com/jerzymaj/hotel_guest_service_system/configuration/TwilioConfig.java
@@ -0,0 +1,21 @@
+package com.jerzymaj.hotel_guest_service_system.configuration;
+
+import com.twilio.Twilio;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TwilioConfig {
+
+    @Value("${app.twilio.account-sid}")
+    private String accountSid;
+
+    @Value("${app.twilio.auth-token}")
+    private String authToken;
+
+    @PostConstruct
+    public void init() {
+        Twilio.init(accountSid, authToken);
+    }
+}
