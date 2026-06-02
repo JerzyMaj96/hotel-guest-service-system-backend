8e07eafcb192ce194c1dc09b2cb017c843b50d80
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-06-02 20:58:07 +0200

    refactor: remove redundant setAuthentication call in login endpoint

--- src/main/java/com/jerzymaj/hotel_guest_service_system/controllers/AuthController.java
+++ src/main/java/com/jerzymaj/hotel_guest_service_system/controllers/AuthController.java
@@ -4,7 +4,6 @@ import com.jerzymaj.hotel_guest_service_system.DTOs.LoginRequest;
 import com.jerzymaj.hotel_guest_service_system.DTOs.RegisterUserDto;
 import com.jerzymaj.hotel_guest_service_system.DTOs.UserDto;
 import com.jerzymaj.hotel_guest_service_system.configuration.JwtProvider;
-import com.jerzymaj.hotel_guest_service_system.models.User;
 import com.jerzymaj.hotel_guest_service_system.services.UserService;
 import com.jerzymaj.hotel_guest_service_system.translator.Translator;
 import jakarta.validation.Valid;
@@ -13,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -53,8 +51,6 @@ public class AuthController {
                         loginRequest.password()
                 )
         );
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
 
         return jwtTokenProvider.generateToken(authentication);
     }
