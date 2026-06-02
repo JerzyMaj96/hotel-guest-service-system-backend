f1d9423df093498230a16afb69e02e521cb50682
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-06-02 13:46:05 +0200

    fix: correct sender and recipient order in issue creation notification

--- src/main/java/com/jerzymaj/hotel_guest_service_system/services/IssueService.java
+++ src/main/java/com/jerzymaj/hotel_guest_service_system/services/IssueService.java
@@ -57,7 +57,7 @@ public class IssueService {
 
         Issue savedIssue = issueRepository.save(issue);
 
-        emailNotificationSender.send(new Notification(user.getEmail(), techEmail, issueCreateRequestDto.title(), issueCreateRequestDto.description()));
+        emailNotificationSender.send(new Notification(techEmail, issueCreateRequestDto.title(), issueCreateRequestDto.description(), user.getEmail()));
 
         return savedIssue;
     }
@@ -82,8 +82,8 @@ public class IssueService {
 
         issue.setStatus(issueStatus);
 
-        emailNotificationSender.send(new Notification(techEmail, issue.getUser().getEmail(), "Issue status updated",
-                "The status of your issue '" + issue.getTitle() + "' has been updated to: " + issueStatus));
+        emailNotificationSender.send(new Notification(issue.getUser().getEmail(), "Issue status updated",
+                "The status of your issue '" + issue.getTitle() + "' has been updated to: " + issueStatus, techEmail));
 
         if (issue.getUser().getPhoneNumber() != null) {
             smsNotificationSender.send(new Notification(issue.getUser().getPhoneNumber(), "Issue status updated",
