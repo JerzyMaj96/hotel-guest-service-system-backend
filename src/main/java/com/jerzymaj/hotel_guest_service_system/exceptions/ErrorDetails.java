6cc50f11597870860084e2e51c63c6e551b36cf8
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-06-03 09:55:56 +0200

    refactor: rename ErrorDetails fields to timestamp, message, path

--- src/main/java/com/jerzymaj/hotel_guest_service_system/exceptions/ErrorDetails.java
+++ src/main/java/com/jerzymaj/hotel_guest_service_system/exceptions/ErrorDetails.java
@@ -2,6 +2,6 @@ package com.jerzymaj.hotel_guest_service_system.exceptions;
 
 import java.time.LocalDateTime;
 
-public record ErrorDetails(LocalDateTime timeStamp, String description,
-                           String details) {
+public record ErrorDetails(LocalDateTime timeStamp, String message,
+                           String path) {
 }
